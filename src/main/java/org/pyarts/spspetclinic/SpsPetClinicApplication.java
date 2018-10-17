3c1f8111a024acc200d0df469de3243c72c20b61
sushildangi <dangi.sushil5@gmail.com>
2018-10-17 17:59:50 +0530

    Initial Project Add

---
+++ src/main/java/org/pyarts/spspetclinic/SpsPetClinicApplication.java
@@ -0,0 +1,12 @@
+package org.pyarts.spspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/org/pyarts/spspetclinic/SpsPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package org.pyarts.spspetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpsPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
