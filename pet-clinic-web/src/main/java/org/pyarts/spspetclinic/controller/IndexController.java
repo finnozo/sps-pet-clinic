03fbf59c71d068f96a3abbd18a6eed3a9082ed35
Sushil Dangi <dangi.sushil5@gmail.com>
2018-10-21 10:03:41 +0530

    Adding Index Page and controller. closes #8.

---
+++ pet-clinic-web/src/main/java/org/pyarts/spspetclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package org.pyarts.spspetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.htm", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
