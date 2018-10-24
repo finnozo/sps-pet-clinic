11b08f565f6c64a824c412d73f1a57eff2b01d53
sushildangi <dangi.sushil5@gmail.com>
2018-10-24 18:01:40 +0530

    Refactor Map impl to abstract ID generation. closes #23

--- pet-clinic-data/src/main/java/org/pyarts/spspetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package org.pyarts.spspetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import org.pyarts.spspetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    private Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    private Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannt be null");
+        }
         return object;
     }
 
@@ -29,4 +35,18 @@ public abstract class AbstractMapService<T, ID> {
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
+
+    private Long getNextId() {
+        long nextId;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            System.err.println(e.getMessage() + " <-- Handled Exception Message --> " + e.getCause());
+            nextId = 1L;
+        }
+
+        return nextId;
+
+    }
+
 }
--- pet-clinic-web/src/main/java/org/pyarts/spspetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/pyarts/spspetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import org.pyarts.spspetclinic.model.Owner;
 import org.pyarts.spspetclinic.model.Vet;
 import org.pyarts.spspetclinic.services.OwnerService;
 import org.pyarts.spspetclinic.services.VetService;
-import org.pyarts.spspetclinic.services.map.OwnerServiceMap;
-import org.pyarts.spspetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -24,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Sushil");
         owner1.setLastName("Dangi");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Sandeep");
         owner2.setLastName("Kumar");
         ownerService.save(owner2);
@@ -38,13 +34,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owner Loaded...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("AXE");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Rahul");
         vet2.setLastName("KRK");
         vetService.save(vet2);
