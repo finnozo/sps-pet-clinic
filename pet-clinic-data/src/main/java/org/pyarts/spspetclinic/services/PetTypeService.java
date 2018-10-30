84da7ff70870ea4f6234df616524b729c1fb5c44
sushildangi <dangi.sushil5@gmail.com>
2018-10-30 16:34:54 +0530

    Added PetType Service closes #29

---
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package org.pyarts.spspetclinic.services;
+
+import org.pyarts.spspetclinic.model.PetType;
+import org.pyarts.spspetclinic.services.util.JpaRepositoryService;
+
+public interface PetTypeService extends JpaRepositoryService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package org.pyarts.spspetclinic.services.map;
+
+import org.pyarts.spspetclinic.model.PetType;
+import org.pyarts.spspetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
