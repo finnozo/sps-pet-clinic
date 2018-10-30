6f2e7c572b8eedacb4dff441532e648323bfb73b
sushildangi <dangi.sushil5@gmail.com>
2018-10-30 16:08:53 +0530

    Added Speciality, associate to Vet . closes #27

--- pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Vet.java
@@ -1,6 +1,16 @@
 package org.pyarts.spspetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
