fddbe955839a371fe255c047ec140d16e124d2b1
Sushil Dangi <dangi.sushil5@gmail.com>
2018-11-03 11:47:35 +0530

    Adding Specialties map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package org.pyarts.spspetclinic.services;
+
+import org.pyarts.spspetclinic.model.Speciality;
+import org.pyarts.spspetclinic.services.util.JpaRepositoryService;
+
+public interface SpecialitiesService extends JpaRepositoryService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,38 @@
+package org.pyarts.spspetclinic.services.map;
+
+import org.pyarts.spspetclinic.model.Speciality;
+import org.pyarts.spspetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
