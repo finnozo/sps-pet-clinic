e5a5ebd03930b552a066a7e473fdba31852837f8
Sushil Dangi <dangi.sushil5@gmail.com>
2018-11-04 10:51:35 +0530

    Update vet with JPA Entity Mapping. closes #37

--- pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package org.pyarts.spspetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package org.pyarts.spspetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
