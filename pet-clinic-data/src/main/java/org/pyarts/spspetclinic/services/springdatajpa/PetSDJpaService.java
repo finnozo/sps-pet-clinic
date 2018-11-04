674b192600253bda8e9b21b8236b8a07d0a69adf
Sushil Dangi <dangi.sushil5@gmail.com>
2018-11-04 12:12:37 +0530

    Added spring data jpa for Pet. closes #43

---
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package org.pyarts.spspetclinic.services.springdatajpa;
+
+import org.pyarts.spspetclinic.model.Pet;
+import org.pyarts.spspetclinic.repositories.PetRepository;
+import org.pyarts.spspetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
