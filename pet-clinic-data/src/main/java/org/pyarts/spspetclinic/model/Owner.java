62a5be6348ee36e2130c5747811ac9e42bd6604b
Sushil Dangi <dangi.sushil5@gmail.com>
2018-11-04 10:41:11 +0530

    Update owner with JPA Entity Mapping. closes #36

--- pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/Owner.java
@@ -1,13 +1,21 @@
 package org.pyarts.spspetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners ")
 public class Owner extends Person {
 
+    @Column(name = "address")
+
     private String address;
+    @Column(name = "city", columnDefinition = "text", length = 500)
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/org/pyarts/spspetclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package org.pyarts.spspetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
